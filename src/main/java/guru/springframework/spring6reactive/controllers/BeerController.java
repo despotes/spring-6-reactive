d351d993cddac7b8150c7061d1f8a524cdcab830
Pedro Medeiros <pietromaddera@gmail.com>
2024-12-26 01:50:53 +0100

    279. Using Path Variables

--- src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
@@ -4,17 +4,26 @@ import guru.springframework.spring6reactive.model.BeerDTO;
 import guru.springframework.spring6reactive.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
 public class BeerController {
     public static final String BEERS_PATH = "/api/v2/beer";
+    public static final String BEERS_PATH_ID = BEERS_PATH + "/{beerId}";
 
     private final BeerService beerService;
+
     @GetMapping(BEERS_PATH)
     Flux<BeerDTO> listBeers() {
         return beerService.listBeers();
+    }
+
+    @GetMapping(BEERS_PATH_ID)
+    Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId) {
+        return beerService.findById(beerId);
     }
 }
